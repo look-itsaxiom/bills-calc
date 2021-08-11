c10a5a2a08808589a15016b5ec1935a7a247a150
Chase Skibeness <chaseskibbs@gmail.com>
2021-08-11 16:41:21 -0700

    added BillListForm and created it's form, starting on logic to call back to parent App

--- src/BillListForm.js
+++ src/BillListForm.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 function BillListForm(props) {
     return (
-        <form>
+        <form onSubmit={props.createBill}>
+            <h2>Enter new bill:</h2>
             <label>Bill Name: <input type="text" name="billName"></input></label>
-            <label>BalanceDue: <input type="number" name="balanceDue"></input></label>
-            <label>Due Date: <input type="datetime-local" name="dueDate"></input></label>
-            <input type="submit" value="Add"></input>
+            <label>BalanceDue: <input type="number" name="balanceDue" step="0.01"></input></label>
+            <label>Due Date: <input type="date" name="dueDate"></input></label>
+            <input type="submit" value="Add" ></input>
         </form>
     )
 }
+
+export default BillListForm;
