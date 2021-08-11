f1c0d55efac1ef1f048ef17d0b26c62d829ed6df
Axiom <itsaxiom.dev@proton.me>
2021-08-11 13:03:43 -0700

    added BillListForm component and basic form

--- src/BillListForm.js
+++ src/BillListForm.js
@@ -1,1 +1,12 @@
 import React from 'react';
+
+function BillListForm(props) {
+    return (
+        <form>
+            <label>Bill Name: <input type="text" name="billName"></input></label>
+            <label>BalanceDue: <input type="number" name="balanceDue"></input></label>
+            <label>Due Date: <input type="datetime-local" name="dueDate"></input></label>
+            <input type="submit" value="Add"></input>
+        </form>
+    )
+}
