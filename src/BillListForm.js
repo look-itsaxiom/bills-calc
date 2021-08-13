af1e33a85272abdc8b5a4cf3b272c90b2dfe1947
Chase Skibeness <chaseskibbs@gmail.com>
2021-08-13 16:12:04 -0700

    form now submits and sends data back to parent, however the createBill function should really receive a constructed object and just update state instead of trying to construct the object to save.

--- src/BillListForm.js
+++ src/BillListForm.js
@@ -1,12 +1,33 @@
 import React from 'react';
 
 function BillListForm(props) {
+    let formValues = {
+
+    };
+
+    const submit = e => {
+        e.preventDefault();
+        props.onClick(formValues);
+    }
+    
+    function handleNameChange(e) {
+        formValues.billName = e.target.value;
+    }
+
+    function handleAmountChange(e) {
+        formValues.amt = e.target.value;
+    }
+
+    function handleDateChange(e) {
+        formValues.dueDate = e.target.value;
+    }
+
     return (
-        <form onSubmit={props.createBill}>
+        <form onSubmit={submit}>
             <h2>Enter new bill:</h2>
-            <label>Bill Name: <input type="text" name="billName"></input></label>
-            <label>BalanceDue: <input type="number" name="balanceDue" step="0.01"></input></label>
-            <label>Due Date: <input type="date" name="dueDate"></input></label>
+            <label>Bill Name: <input type="text" name="billName" value={props.billName} onChange={handleNameChange}></input></label>
+            <label>BalanceDue: <input type="number" name="balanceDue" step="0.01" value={props.amt} onChange={handleAmountChange}></input></label>
+            <label>Due Date: <input type="date" name="dueDate" value={props.dueDate} onChange={handleDateChange}></input></label>
             <input type="submit" value="Add" ></input>
         </form>
     )
