f2ef7b812523ccff5a49520c83f26c6f2ca4943f
Chase Skibeness <chaseskibbs@gmail.com>
2021-08-16 11:20:18 -0700

    changed app title, added totals to tables

--- src/BillListDisplay.js
+++ src/BillListDisplay.js
@@ -2,19 +2,9 @@ import React from 'react';
 
 function BillListDisplay(props) {
 
-    let unpaidBillList = props.billsList.filter(bill => !bill.paid).sort((a,b) => a.dueDate - b.dueDate).map(bill => 
-        (<tr key={bill.name}>
-            <td>{bill.name}</td>
-            <td>{bill.balanceDue}</td>
-        {bill.dueDate <= new Date() ? (<td style={{ "backgroundColor": "hsla(0, 100%, 38%, 0.72)"}}>{bill.dueDate.toLocaleDateString()}</td>) : (<td>{bill.dueDate.toLocaleDateString()}</td>)}
-        </tr>));
+    let unpaidBillList = props.billsList.filter(bill => !bill.paid).sort((a,b) => a.dueDate - b.dueDate)
 
-    let paidBillList = props.billsList.filter(bill => bill.paid).sort((a, b) => a.dueDate - b.dueDate).map(bill =>
-    (<tr key={bill.name}>
-        <td>{bill.name}</td>
-        <td>{bill.balanceDue}</td>
-        <td>{bill.dueDate.toLocaleDateString()}</td>
-    </tr>));
+    let paidBillList = props.billsList.filter(bill => bill.paid).sort((a, b) => a.dueDate - b.dueDate)
 
     return (
         <>
@@ -28,8 +18,21 @@ function BillListDisplay(props) {
                     </tr>
                 </thead>
                 <tbody>
-                    {unpaidBillList}
+                    {unpaidBillList.map(bill => ( 
+                        <tr key={bill.name}>
+                            <td>{bill.name}</td>
+                            <td>{bill.balanceDue}</td>
+                            {bill.dueDate <= new Date() ? (<td style={{ "backgroundColor": "hsla(0, 100%, 38%, 0.72)" }}>{bill.dueDate.toLocaleDateString()}</td>) : (<td>{bill.dueDate.toLocaleDateString()}</td>)}
+                        </tr>
+                        )
+                    )}
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Total Owed</th>
+                        <td>{unpaidBillList.reduce((acc, curr) => acc + curr.balanceDue, 0).toFixed(2)}</td>
+                    </tr>
+                </tfoot>
             </table>
             
             <table>
@@ -42,8 +45,21 @@ function BillListDisplay(props) {
                     </tr>
                 </thead>
                 <tbody>
-                    {paidBillList}
+                    {paidBillList.map(bill => (
+                        <tr key={bill.name}>
+                            <td>{bill.name}</td>
+                            <td>{bill.balanceDue}</td>
+                            <td>{bill.dueDate.toLocaleDateString()}</td>
+                        </tr>
+                        )
+                    )}
                 </tbody>
+                <tfoot>
+                    <tr>
+                        <th>Total Paid</th>
+                        <td>{paidBillList.reduce((acc, curr) => acc + curr.balanceDue, 0).toFixed(2)}</td>
+                    </tr>
+                </tfoot>
             </table>
         </>
     )
