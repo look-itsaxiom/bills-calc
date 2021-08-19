bd7b50ede8f3f30123e821167012bc9705b6081a
Chase Skibeness <chaseskibbs@gmail.com>
2021-08-19 11:58:11 -0700

    integrated FullCalendar, auto displays bill events

--- src/BillListSchedule.js
+++ src/BillListSchedule.js
@@ -1,5 +1,20 @@
 import React from 'react';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
 
 export default function BillListSchedule(props) {
-    
+    const billsToEvents = (billsList) => {
+        return billsList.map(bill => ({
+            id: billsList.indexOf(bill),
+            title: bill.name,
+            date: bill.dueDate,
+            backgroundColor: (bill.paid ? '#D6FF8E' : "hsla(0, 100%, 38%, 0.72)"),
+            textColor: "black",
+            allDay: true
+
+        }))
+    }
+    return (
+        <FullCalendar plugins={[ dayGridPlugin ]} initialView="dayGridMonth" events={billsToEvents(props.billsList)} />
+    )
 }
