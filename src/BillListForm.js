f71b72d977aebfb44421856f2537a0fb623114d4
Chase Skibeness <chaseskibbs@gmail.com>
2021-08-25 20:35:47 -0700

    started Form change to bootstrap elements

--- src/BillListForm.js
+++ src/BillListForm.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import FloatingLabel from 'react-bootstrap/FloatingLabel';
 
 function BillListForm(props) {
     let formValues = {
@@ -35,13 +38,25 @@ function BillListForm(props) {
     }
 
     return (
-        <form onSubmit={submit} id="new-bill" autoComplete="off">
+        <Form onSubmit={submit} id="new-bill" autoComplete="off">
             <h2>Enter new bill:</h2>
-            <label>Bill Name: <input required type="text" name="billName" onChange={handleNameChange}></input></label>
-            <label>BalanceDue: <input required type="number" name="balanceDue" step="0.01" onChange={handleAmountChange}></input></label>
-            <label>Due Date: <input required type="date" name="dueDate" onChange={handleDateChange}></input></label>
-            <input type="submit" value="Add"></input>
-        </form>
+            <Form.Group>
+                <FloatingLabel label="Bill Name:" controlId="billName" className="mb-3">
+                    <Form.Control required type="text" name="billName" onChange={handleNameChange}></Form.Control>
+                </FloatingLabel>
+                <FloatingLabel label="Balance Due:" controlId="balanceDue" className="mb-3">
+                    <Form.Control required type="number" step="0.01" name="balanceDue" onChange={handleAmountChange}></Form.Control>
+                </FloatingLabel>
+                <FloatingLabel label="Due Date:" controlId="dueDate" className="mb-3">
+                    <Form.Control required type="date" name="dueDate" onChange={handleDateChange}></Form.Control>
+                </FloatingLabel>
+                <Button variant="primary" type="submit">Add</Button>
+            </Form.Group>
+            {/*<label>Bill Name: <input required type="text" name="billName" onChange={handleNameChange}></input></label>*/}
+            {/*<label>BalanceDue: <input required type="number" name="balanceDue" step="0.01" onChange={handleAmountChange}></input></label>*/}
+            {/* <label>Due Date: <input required type="date" name="dueDate" onChange={handleDateChange}></input></label>
+            <input type="submit" value="Add"></input> */}
+        </Form>
     )
 }
 
